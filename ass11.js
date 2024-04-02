f86a9d2cd4d54bd31979d588633d2718d10b9d5d
RestorerBarisereden <restorerzorzor@gmail.com>
2024-04-02 23:48:11 +0100

    first push

---
+++ ass11.js
@@ -0,0 +1,13 @@
+let age = 16
+let score = '80%'
+if ("age >= 16 && score >= 80%"){
+    console.log("Drivers license approved");
+}
+else if("age < 16 && score >= 90%"){
+    console.log("Learner's permit granted");
+}
+else {
+    console.log("Drivers license access denied");
+}
+
+
